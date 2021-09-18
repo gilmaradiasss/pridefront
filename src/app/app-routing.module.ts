92cbabfa763ee4937d848877b9ffa74aa4987644
alfarth.helena@gmail.com <alfarth.helena@gmail.com>
2021-09-17 21:37:40 -0300

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PatientsComponent } from './patients/patients.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
   {path: 'profissionais',component: ProfessionalsComponent},
+  {path: 'fale-com-a-gente',component: ContactComponent},
   {path: 'pacientes',component: PatientsComponent}];
 
 @NgModule({
